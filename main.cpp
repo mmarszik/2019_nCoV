b9b4d600db7d7548cc61a9c8a7f701caeeed0556
mmarszik <mmarszik@gmail.com>
2020-02-08 01:27:55 +0100

    .

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ void initParams( TRnd &rnd, ftyp params[SIZE_PRMS] ) {
 }
 
 static void chaos( TRnd &rnd, ftyp params[SIZE_PRMS], cftyp step ) {
-    if( step >= 1 ) {
+    if( step >= 0.1 ) {
         switch( rnd() % 7 ) {
             case 0: params[0] = rnd.getFloat(-1000,+1000); break;
             case 1: params[1] = rnd.getFloat(-10,+10); break;
@@ -70,11 +70,11 @@ static ftyp compute(
     Solve &best
 ) {
     static cftyp data[SIZE_DATA] = {282,314,579,843,1337,2014,2798,4593,6065,7818,9826,11953,14557,17391,20630,24554,28276,31481};
-    ftyp bigPenal = 1E-20;
+    ftyp bigPenal = 1E-14;
     ftyp e = eval( best.params , data , bigPenal );
     Solve solve = best;
-    cftyp step_start = 1.0;
-    cftyp step_end   = 0.005;
+    cftyp step_start = 0.1;
+    cftyp step_end   = 0.001;
     cltyp part       = 0xFFFFFu;
     cltyp loops      = part*50u;
     ultyp full_rand  = part*4u;
